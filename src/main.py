10fda2354c9dd1070a4567c99a2260dc4f719c28
john-lohoba <jsscadden@gmail.com>
2026-03-24 19:08:03 +0000

    Add main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+
+def main():
+    example_node = TextNode(
+        "This is some anchor text", TextType.LINK, "https://boot.dev"
+    )
+    print(example_node)
+
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -26,4 +26,4 @@ class TextNode:
         return False
 
     def __repr__(self):
-        return f"TextNode({self.text}, {self.tex_type}, {self.url})"
+        return f"TextNode({self.text}, {self.tex_type.value}, {self.url})"
