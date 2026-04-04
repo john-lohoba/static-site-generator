0e74e80fc3ce71906b29fa9f36b05ed3d10b8063
john-lohoba <jsscadden@gmail.com>
2026-04-04 09:49:32 +0100

    Add markdown to blocks

--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -5,6 +5,7 @@ from split_nodes import (
     split_nodes_image,
     split_nodes_link,
     text_to_textnodes,
+    markdown_to_blocks,
 )
 from textnode import TextNode, TextType
 
@@ -108,4 +109,24 @@ class TestSplitNodes(unittest.TestCase):
                 TextNode("link", TextType.LINK, "https://boot.dev"),
             ],
             nodes,
+        )
+
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
         )
