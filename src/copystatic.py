28fcf036918c712edb5eff8746a8e2e462640dd1
john-lohoba <jsscadden@gmail.com>
2026-04-07 21:43:31 +0100

    Add generate content

--- src/copystatic.py
+++ src/copystatic.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+from block_to_html import markdown_to_html_node
+from markdown_blocks import BlockType, markdown_to_blocks, block_to_block_type
 
 
 def copy_files_recursive(source_dir_path, dest_dir_path):
@@ -15,3 +17,32 @@ def copy_files_recursive(source_dir_path, dest_dir_path):
         else:
             copy_files_recursive(from_path, dest_path)
 
+
+def extract_title(markdown):
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        if block.startswith("# ") and block_to_block_type(block) == BlockType.HEADING:
+            return block.strip("#").strip()
+    raise Exception("Markdown has no h1 heading")
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    from_path_content = open(from_path).read()
+    template_path_content = open(template_path).read()
+
+    path_content_to_html = markdown_to_html_node(from_path_content).to_html()
+    page_title = extract_title(from_path_content)
+
+    copy_template = template_path_content.replace("{{ Title }}", page_title)
+    copy_template = copy_template.replace("{{ Content }}", path_content_to_html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    os.makedirs(dest_dir_path, exist_ok=True)
+    # print("Rewriting index.html...")
+    file = open(dest_path, "w")
+    file.write(copy_template)
+    file.close()
+    # print("Template edited.")
+
+    return
